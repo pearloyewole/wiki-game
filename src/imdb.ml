1962105f779baad4910c3971fdbcb9baf0afdc7f
Pearl Oyewole <pearloyewole1@gmail.com>
2025-06-25 19:36:05 +0000

    interstate works

--- src/imdb.ml
+++ src/imdb.ml
@@ -1,16 +1,30 @@
 open! Core
 
+let get_property node =
+  let open Soup in
+  match attribute "property" node with Some content -> content | None -> ""
+;;
+
+let _get_content node =
+  let open Soup in
+  match attribute "content" node with Some credit -> credit | None -> ""
+;;
+
 (* [get_credits] should take the contents of an IMDB page for an actor and return a list
    of strings containing that actor's main credits. *)
 let get_credits contents : string list =
-  ignore (contents : string);
-  failwith "TODO"
+  let open Soup in
+  let document_node = parse contents in
+  let credit_list_nodes = document_node $$ "meta" |> to_list in
+  List.map credit_list_nodes ~f:(fun a -> get_property a)
 ;;
 
 let print_credits_command =
   let open Command.Let_syntax in
   Command.basic
-    ~summary:"given an IMDB page for an actor, print out a list of their main credits"
+    ~summary:
+      "given an IMDB page for an actor, print out a list of their main \
+       credits"
     [%map_open
       let how_to_fetch, resource = File_fetcher.param in
       fun () ->
@@ -19,5 +33,7 @@ let print_credits_command =
 ;;
 
 let command =
-  Command.group ~summary:"imdb commands" [ "print-credits", print_credits_command ]
+  Command.group
+    ~summary:"imdb commands"
+    [ "print-credits", print_credits_command ]
 ;;
--- src/interstate.ml
+++ src/interstate.ml
@@ -1,4 +1,44 @@
 open! Core
+module City = String
+
+module Network = struct
+  (* We can represent our social network graph as a set of connections, where a connection
+     represents a friendship between two people. *)
+  module Connection = struct
+    module T = struct
+      type t = City.t * City.t [@@deriving compare, sexp]
+    end
+
+    include Comparable.Make (T)
+
+    let rec pair_strings lst =
+      match lst with
+      | x :: y :: rest -> (x, y) :: pair_strings (y :: rest)
+      | _ -> []
+    ;;
+
+    let create_city_pairs s =
+      match String.split_on_chars ~on:[ ',' ] s with
+      | _route_name :: cities -> pair_strings cities
+      | [] -> []
+    ;;
+
+    let of_string s =
+      create_city_pairs s
+      |> List.map ~f:(fun (a, b) -> City.of_string a, City.of_string b)
+    ;;
+  end
+
+  type t = Connection.Set.t [@@deriving sexp_of]
+
+  let of_file input_file =
+    In_channel.read_lines (File_path.to_string input_file)
+    |> List.concat_map ~f:(fun s ->
+      Connection.of_string s
+      |> List.concat_map ~f:(fun (a, b) -> [ a, b; b, a ]))
+    |> Connection.Set.of_list
+  ;;
+end
 
 let load_command =
   let open Command.Let_syntax in
@@ -9,25 +49,46 @@ let load_command =
         flag
           "input"
           (required File_path.arg_type)
-          ~doc:"FILE a file listing interstates and the cities they go through"
+          ~doc:
+            "FILE a file listing interstates and the cities they go through"
       in
       fun () ->
-        ignore (input_file : File_path.t);
-        failwith "TODO"]
+        let network = Network.of_file input_file in
+        printf !"%{sexp: Network.t}\n" network]
 ;;
+
+module G = Graph.Imperative.Graph.Concrete (City)
+
+module Dot = Graph.Graphviz.Dot (struct
+    include G
+
+    (* These functions can be changed to tweak the appearance of the generated
+       graph. Check out the ocamlgraph graphviz API
+       (https://github.com/backtracking/ocamlgraph/blob/master/src/graphviz.mli) for
+       examples of what values can be set here. *)
+    let edge_attributes _ = [ `Dir `None ]
+    let default_edge_attributes _ = []
+    let get_subgraph _ = None
+    let vertex_attributes v = [ `Shape `Box; `Label v; `Fillcolor 1000 ]
+    let vertex_name v = v
+    let default_vertex_attributes _ = []
+    let graph_attributes _ = []
+  end)
 
 let visualize_command =
   let open Command.Let_syntax in
   Command.basic
     ~summary:
-      "parse a file listing interstates and generate a graph visualizing the highway \
-       network"
+      "parse a file listing interstates and generate a graph visualizing \
+       the highway network"
     [%map_open
       let input_file =
         flag
           "input"
           (required File_path.arg_type)
-          ~doc:"FILE a file listing all interstates and the cities they go through"
+          ~doc:
+            "FILE a file listing all interstates and the cities they go \
+             through"
       and output_file =
         flag
           "output"
@@ -35,8 +96,15 @@ let visualize_command =
           ~doc:"FILE where to write generated graph"
       in
       fun () ->
-        ignore (input_file : File_path.t);
-        ignore (output_file : File_path.t);
+        let network = Network.of_file input_file in
+        let graph = G.create () in
+        Set.iter network ~f:(fun (city1, city2) ->
+          (* [G.add_edge] auomatically adds the endpoints as vertices in the graph if
+             they don't already exist. *)
+          G.add_edge graph city1 city2);
+        Dot.output_graph
+          (Out_channel.create (File_path.to_string output_file))
+          graph;
         printf !"Done! Wrote dot file to %{File_path}\n%!" output_file]
 ;;
 
